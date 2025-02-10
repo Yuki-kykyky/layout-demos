8c044f9ebf5e9ba55ebf7f998eb58154dd12accc
yuki <kyoiii0219@163.com>
2025-02-10 16:32:58 +0800

    update banner-section

--- app/components/banner-section.tsx
+++ app/components/banner-section.tsx
@@ -3,15 +3,36 @@ import { Box, Typography, Button, Container, Paper } from "@mui/material";
 
 export default function BannerSection() {
   return (
-    <Container maxWidth="lg">
+    <Container
+      maxWidth="lg"
+      sx={{
+        backgroundImage: "url(/world-map.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        position: "relative",
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(255, 255, 255, 0.85)", // 添加半透明白色遮罩
+          zIndex: 1,
+        },
+      }}
+    >
       <Box
         sx={{
           display: "grid",
-          gridTemplateColumns: { xs: "1fr", md: "2fr 3fr" },
+          gridTemplateColumns: { xs: "1fr", md: "1fr 1fr" },
           gap: 8,
           alignItems: "center",
-          minHeight: "400px",
+          minHeight: "80vh",
           pt: { xs: 8, md: 0 },
+          position: "relative",
+          zIndex: 2, // 确保内容在遮罩层之上
         }}
       >
         {/* Left Text Section */}
@@ -43,16 +64,15 @@ export default function BannerSection() {
         </Box>
 
         {/* Right Image Section */}
-        <Box sx={{ position: "relative", height: "100%", minHeight: "500px" }}>
-          {/* Image Cards */}
+        <Box sx={{ position: "relative", height: "600px" }}>
           <Paper
             elevation={2}
             sx={{
               position: "absolute",
               top: "10%",
               left: "0%",
-              width: "280px",
-              height: "320px",
+              width: "250px",
+              height: "400px",
               borderRadius: "24px",
               overflow: "hidden",
               bgcolor: "grey.100",
@@ -63,9 +83,9 @@ export default function BannerSection() {
             sx={{
               position: "absolute",
               top: "5%",
-              right: "10%",
+              right: "0%",
               width: "240px",
-              height: "240px",
+              height: "300px",
               borderRadius: "24px",
               overflow: "hidden",
               bgcolor: "grey.100",
@@ -75,9 +95,9 @@ export default function BannerSection() {
             elevation={2}
             sx={{
               position: "absolute",
-              bottom: "15%",
-              right: "5%",
-              width: "200px",
+              bottom: "5%",
+              right: "0%",
+              width: "240px",
               height: "200px",
               borderRadius: "24px",
               overflow: "hidden",
