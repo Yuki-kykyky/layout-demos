621785ad32ae64a3c7afed936daeaadcf2f968bd
yuki <kyoiii0219@163.com>
2025-02-11 10:10:08 +0800

    init footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,20 @@ import TopDestinations from "./components/top-destinations";
 import LatestStories from "./components/latest-stories";
 import Highlights from "./components/highlights";
 import EmailConnect from "./components/email-connect";
+import Footer from "./components/footer";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex-1 flex flex-col gap-8 p-8 sm:p-20">
         <BannerSection />
         <TopDestinations />
         <LatestStories />
         <Highlights />
         <EmailConnect />
       </main>
+      <Footer />
     </div>
   );
 }
