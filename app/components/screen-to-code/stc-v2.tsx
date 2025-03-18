94abf8f7b51ea90524658d48c2ffda8f6947fa6f
yuki <kyoiii0219@163.com>
2025-03-18 16:07:08 +0800

    feat: update shared layout

--- app/components/screen-to-code/stc-v2.tsx
+++ app/components/screen-to-code/stc-v2.tsx
@@ -1,35 +1,27 @@
-import React from "react";
-import { Box, Container } from "@mui/material";
-import { Header } from "@/app/components/screen-to-code/components/v2/header";
-import { HeaderBanner } from "@/app/components/screen-to-code/components/v2/header-banner";
-import { VisionSection } from "@/app/components/screen-to-code/components/v2/vision-section";
-import { PropertiesSection } from "@/app/components/screen-to-code/components/v2/properties-section";
+import { BlogsSection } from "@/app/components/screen-to-code/components/v2/blogs-section";
 import { FeaturesSection } from "@/app/components/screen-to-code/components/v2/features-section";
+import { HeaderBanner } from "@/app/components/screen-to-code/components/v2/header-banner";
 import { HorizontalCardSection } from "@/app/components/screen-to-code/components/v2/horizontal-card-section";
 import { HowItWorkSection } from "@/app/components/screen-to-code/components/v2/how-it-work-section";
-import { WhyChooseSection } from "@/app/components/screen-to-code/components/v2/why-choose-section";
+import { PropertiesSection } from "@/app/components/screen-to-code/components/v2/properties-section";
 import { TestimonialsSection } from "@/app/components/screen-to-code/components/v2/testimonials-section";
-import { BlogsSection } from "@/app/components/screen-to-code/components/v2/blogs-section";
-import { FooterBanner } from "@/app/components/screen-to-code/components/v2/footer-banner";
-import { Footer } from "@/app/components/screen-to-code/components/v2/footer";
+import { VisionSection } from "@/app/components/screen-to-code/components/v2/vision-section";
+import { WhyChooseSection } from "@/app/components/screen-to-code/components/v2/why-choose-section";
+import { SharedLayout } from "../shared-layout";
+import React from "react";
 
 export const STCv2 = () => {
   return (
-    <Box>
-      <Container maxWidth={"lg"}>
-        <Header />
-        <HeaderBanner />
-        <VisionSection />
-        <PropertiesSection />
-        <FeaturesSection />
-        <HorizontalCardSection />
-        <HowItWorkSection />
-        <WhyChooseSection />
-        <TestimonialsSection />
-        <BlogsSection />
-        <FooterBanner />
-        <Footer />
-      </Container>
-    </Box>
+    <SharedLayout>
+      <HeaderBanner />
+      <VisionSection />
+      <PropertiesSection />
+      <FeaturesSection />
+      <HorizontalCardSection />
+      <HowItWorkSection />
+      <WhyChooseSection />
+      <TestimonialsSection />
+      <BlogsSection />
+    </SharedLayout>
   );
 };
--- app/components/shared-layout.tsx
+++ app/components/shared-layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "@/app/components/screen-to-code/components/v2/header";
 import { Footer } from "@/app/components/screen-to-code/components/v2/footer";
 import { Container } from "@mui/material";
+import { FooterBanner } from "./screen-to-code/components/v2/footer-banner";
 
 interface SharedLayoutProps {
   children: React.ReactNode;
@@ -9,10 +10,13 @@ interface SharedLayoutProps {
 
 export function SharedLayout({ children }: SharedLayoutProps) {
   return (
-    <Container maxWidth={"lg"}>
-      <Header />
-      <main className="flex-grow">{children}</main>
+    <>
+      <Container maxWidth={"lg"}>
+        <Header />
+        <main className="flex-grow">{children}</main>
+        <FooterBanner />
+      </Container>
       <Footer />
-    </Container>
+    </>
   );
 }
